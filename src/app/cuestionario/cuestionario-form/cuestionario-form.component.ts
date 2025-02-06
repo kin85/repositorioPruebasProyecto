c1d1d9510f5b48cd6ee6e4a0009ceecefae50ed2
Joaquin <quino.l85@gmail.com>
2025-02-06 20:34:22 +0100

    preCuestionario

--- src/app/cuestionario/cuestionario-form/cuestionario-form.component.ts
+++ src/app/cuestionario/cuestionario-form/cuestionario-form.component.ts
@@ -1,25 +1,55 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import {FormArray,FormBuilder,FormControl,FormGroup,ReactiveFormsModule,Validators} from '@angular/forms';
 import { BbddService } from '../../services/BBDD.service';
 
 @Component({
   selector: 'app-cuestionario-form',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './cuestionario-form.component.html',
   styleUrls: ['./cuestionario-form.component.css']
 })
 export class CuestionarioFormComponent implements OnInit {
+  preguntas: string[] = [];
+  preCuestionarioForm: FormGroup;
 
-  public cuestionario: any;
-  public preguntas: any[] = [];
-
-  constructor(private bbddService: BbddService) {}
-
-  ngOnInit() {
-    const id = 1; 
-    this.bbddService.getCuestionarioById(id).subscribe(data => {
-      this.cuestionario = data;
-      this.preguntas = data.preguntas;
+  constructor(private bbddService: BbddService,private formBuilder: FormBuilder) {
+    this.preCuestionarioForm = this.formBuilder.group({
+      nombre: ['', [Validators.required]],
+      telefono: ['', [Validators.required]]
     });
   }
+
+  ngOnInit() {
+    this.preguntas = [
+      '¿Cuál es tu nivel de experiencia en el campo?',
+      '¿Cuántos años has trabajado en esta industria?',
+      '¿Qué habilidades específicas has desarrollado en tu carrera?',
+      '¿Has trabajado en proyectos similares antes?',
+      '¿Qué te motiva a trabajar en este campo?'
+    ];
+  }
+
+  get strNombreValid() {
+    if (this.preCuestionarioForm.get('nombre')?.untouched) {
+      return ''
+    } else if(this.preCuestionarioForm.get('nombre')?.touched && this.preCuestionarioForm.get('nombre')?.valid){
+      return 'is-valid'
+    } else {
+      return 'is-invalid'
+    }
+  }
+
+  get strTelefonoValid() {
+    if (this.preCuestionarioForm.get('telefono')?.untouched) {
+      return ''
+    } else if(this.preCuestionarioForm.get('telefono')?.touched && this.preCuestionarioForm.get('telefono')?.valid){
+      return 'is-valid'
+    } else {
+      return 'is-invalid'
+    }
+  }
+
+  
 }
+
